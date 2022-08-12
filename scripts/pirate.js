dece854c03bc384dca5f62312e2081291a7df7ce
João Vitor Pessoa <joaovitorpessoa81@gmail.com>
2022-08-11 23:05:16 -0300

    chore: add faker

--- scripts/pirate.js
+++ scripts/pirate.js
@@ -1,47 +1,47 @@
-const path = require("path");
-const fs = require("fs/promises");
+const path = require('path')
+const fs = require('fs/promises')
 
-const DIRECTORY_TARGET = __dirname + "/../docs/01";
+const DIRECTORY_TARGET = path.join(__dirname, '/../docs/ref/02')
 
 function sanitizeFilename(directory) {
-  return directory.replace(/\.html,?$/, "").replace(/\d+\.(\d?)+ /, "");
+  return directory.replace(/\.html,?$/, '').replace(/\d+\.(\d?)+ /, '')
 }
 
 function filterByHtml(directories) {
-  return directories.filter((directory) => directory.endsWith("html"));
+  return directories.filter((directory) => directory.endsWith('html'))
 }
 
 function getLink(html) {
   const urlRgx =
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g
 
   try {
-    const link = urlRgx.exec(html)[0];
-    return link;
+    const link = urlRgx.exec(html)[0]
+    return link
   } catch (error) {}
 }
 
 async function generateOutput(htmlFilenames) {
-  let output = "";
+  let output = ''
   await Promise.all(
     htmlFilenames.map(async (filename) => {
-      const filePath = path.join(DIRECTORY_TARGET, filename);
-      const _filename = sanitizeFilename(filename);
-      const html = (await fs.readFile(filePath)).toString();
-      const link = getLink(html);
+      const filePath = path.join(DIRECTORY_TARGET, filename)
+      const _filename = sanitizeFilename(filename)
+      const html = (await fs.readFile(filePath)).toString()
+      const link = getLink(html)
       if (link) {
-        output += `[${_filename}](${link})\n`;
+        output += `[${_filename}](${link})\n`
       }
     })
-  );
-  return output;
+  )
+  return output
 }
 
 async function main() {
-  const htmlFiles = filterByHtml(await fs.readdir(DIRECTORY_TARGET));
-  const output = await generateOutput(htmlFiles);
-  console.log(output);
-  return 0;
+  const htmlFiles = filterByHtml(await fs.readdir(DIRECTORY_TARGET))
+  const output = await generateOutput(htmlFiles)
+  console.log(output)
+  return 0
 }
 
-main();
+main()
